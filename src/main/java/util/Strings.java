38fa555ef81b846a3070f4f4c7d62bb3478dd188
Andrea Falconi <andrea.falconi@gmail.com>
2015-09-14 11:12:29 +0200

    add util to write strings using print writer

--- src/main/java/util/Strings.java
+++ src/main/java/util/Strings.java
@@ -1,5 +1,11 @@
 package util;
 
+import static java.util.Objects.requireNonNull;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.function.Consumer;
+
 /**
  * String utilities
  */
@@ -14,5 +20,22 @@ public class Strings {
         return x == null || x.isEmpty();
     }
     
+    /**
+     * Gives a {@link Consumer} a {@link PrintWriter} to write some text to.
+     * @param writer the consumer that will produce the text.
+     * @return the text produced by the consumer.
+     * @throws NullPointerException if the argument is {@code null}.
+     */
+    public static String write(Consumer<PrintWriter> writer) {
+        requireNonNull(writer, "writer");
+        
+        StringWriter buffer = new StringWriter();
+        PrintWriter out = new PrintWriter(buffer);
+        
+        writer.accept(out);
+        out.flush();
+        
+        return buffer.toString();
+    }
     
 }
