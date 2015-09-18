2231adaa3ecb94fc89c1b6908f08603288f36108
Andrea Falconi <andrea.falconi@gmail.com>
2015-09-18 11:01:34 +0200

    improve char encoding javadoc

--- src/main/java/util/servlet/http/CharEncodingFilter.java
+++ src/main/java/util/servlet/http/CharEncodingFilter.java
@@ -29,8 +29,23 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServletResponseWrapper;
 
 /**
- * Sets character encoding for servlets that read/write characters from/to the
- * request/response body if none has been specified.
+ * <p>
+ * Sets character encoding for servlets that read/write characters from/to 
+ * the request/response if none has been specified.
+ * This filter is useful when most or all web components in the application
+ * handle text in a given character encoding only (e.g. UTF-8); using this 
+ * filter explicit setting of the character encoding in each component can 
+ * then be avoided.
+ * </p>
+ * <p>Note that in order to avoid interference (as much as possible) with other
+ * components in the HTTP processing pipeline, this filter delays the setting of
+ * the character encoding until the very last moment it is possible to do it. 
+ * (According to the servlet spec, for requests, this is just before parameters
+ * are read or the body is read; for responses, it is just before the body is 
+ * written.) In fact, a request/response handler downstream may make a decision
+ * as to what encoding to use based on the absence of the character encoding,
+ * which is why this filter holds off setting it. 
+ * </p>
  */
 public class CharEncodingFilter implements Filter {
     
