8f41d1a045fbc713eaab847ecab413eeabfe80f2
Andrea Falconi <andrea.falconi@gmail.com>
2015-09-21 11:42:34 +0200

    fix web config for defaulting string converter to utf-8

--- src/main/java/app/config/WebWiring.java
+++ src/main/java/app/config/WebWiring.java
@@ -1,11 +1,13 @@
 package app.config;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -23,14 +25,24 @@ import app.web.HomeController;
 @Profile(Profiles.WebApp)
 public class WebWiring extends WebMvcConfigurerAdapter {
 
+    private void setStringConverterMediaTypes(HttpMessageConverter<?> x) {
+        StringHttpMessageConverter converter = (StringHttpMessageConverter) x;
+        List<MediaType> mediaTypes = Arrays.asList(
+                new MediaType("text", "plain", StandardCharsets.UTF_8), 
+                MediaType.ALL);
+        converter.setSupportedMediaTypes(mediaTypes);
+    }
+    
     @Override 
     public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.add(new StringHttpMessageConverter(StandardCharsets.UTF_8));
+        converters.stream()
+        .filter(x -> x instanceof StringHttpMessageConverter)
+        .forEach(this::setStringConverterMediaTypes);
     }
     /* NB this overrides the default string converter which uses ISO-8859-1 as
      * default character encoding. The default encoding is what this converter
-     * uses if the response content type has no charset parameter.
-     * So with this set up, we can produce response strings without having to
-     * specify UTF-8 as charset every time.
+     * uses if the request doesn't specify one.
+     * So with this set up, we can produce response strings defaulted to UTF-8
+     * without having to specify UTF-8 as charset every time.
      */
 }
