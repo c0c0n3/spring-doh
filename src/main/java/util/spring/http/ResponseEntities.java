5ce25a172c93f21c2763270b3059aefa087f80f7
Andrea Falconi <andrea.falconi@gmail.com>
2015-09-22 10:29:13 +0200

    add javadoc to response entities

--- src/main/java/util/spring/http/ResponseEntities.java
+++ src/main/java/util/spring/http/ResponseEntities.java
@@ -4,9 +4,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-
+/**
+ * Helper methods for {@link ResponseEntity}'s.
+ */
 public class ResponseEntities {
 
+    /**
+     * Creates a new 404 response with an optional body.
+     * If called with no arguments as in {@code _404()}, the response will have
+     * an empty body; if a {@code body} argument is given, it will be written
+     * to the response body using the configured Spring MVC message converters.
+     * @param body optional response body.
+     * @return a new 404 response.
+     */
     @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Not Found")
     @SafeVarargs
     public static <T> ResponseEntity<T> _404(T...body) {
