3355c84f68845787b7a0e6c22b343029c7ee1433
Andrea Falconi <andrea.falconi@gmail.com>
2015-09-30 16:37:20 +0200

    minor changes

--- src/main/java/util/Arrayz.java
+++ src/main/java/util/Arrayz.java
@@ -237,19 +237,6 @@ public class Arrayz<A> {  // avoids conflicts with JDK Arrays class.
 
         return interspersed;
     }
-    /* [a, /, /, null, c]
-     * 
-     * 0 1 2 3 4
-     * a / b / c
-     * 0   1   2
-     */
-    
-    // 0 1 2 3 4 5 6 7 8
-    // a , b , c , d , e
-    // 0   1   2   3   4
-    
-    // 0 1 2 3
-    // 0 0 1 2
     
     /**
      * Same as {@link Streams#pruneNull(List) Streams.pruneNull} but operating
